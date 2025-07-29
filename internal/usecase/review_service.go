dda1b3618cd5dd783241084037fa7e67f3985a2c
vandannandwana <nandviky4@gmail.com>
2025-07-29 16:25:29 +0530

    review service done without validation

--- internal/usecase/review_service.go
+++ internal/usecase/review_service.go
@@ -1,13 +1,16 @@
 package usecase
 
-import "github.com/vandannandwana/MovieReviewApp/internal/domain"
+import (
+	"github.com/vandannandwana/MovieReviewApp/internal/delivery/http/dto"
+	"github.com/vandannandwana/MovieReviewApp/internal/domain"
+)
 
 type ReviewService interface {
 	CreateReview(review *domain.Review) error
-	GetReviewById(id int64) (*domain.Review, error)
-	GetReviewByMovieId(id int64) (*domain.Review, error)
-	GetReviewByUserEmailId(email string) (*domain.Review, error)
-	UpdateReview(review *domain.Review) error
+	GetReviewById(id int64) (*dto.ReviewResponse, error)
+	GetReviewByMovieId(id int64) ([]dto.ReviewResponse, error)
+	GetReviewByUserEmailId(email string) ([]dto.ReviewResponse, error)
+	UpdateReview(review *domain.Review, reviewId int64) error
 	DeleteReview(reviewId int64) error
 }
 
@@ -20,20 +23,61 @@ func NewReviewService(reviewRepo domain.ReviewRepository) ReviewService {
 }
 
 func (s *reviewService) CreateReview(review *domain.Review) error {
+
+	err := s.reviewRepo.New(review)
+
+	if err != nil{
+		return err
+	}
+
 	return nil
 }
-func (s *reviewService) GetReviewById(id int64) (*domain.Review, error) {
-	return nil, nil
+func (s *reviewService) GetReviewById(id int64) (*dto.ReviewResponse, error) {
+
+	review, err := s.reviewRepo.GetReviewById(id)
+
+	if err != nil{
+		return  nil, err
+	}
+
+	return review, nil
 }
-func (s *reviewService) GetReviewByMovieId(id int64) (*domain.Review, error) {
-	return nil, nil
+func (s *reviewService) GetReviewByMovieId(movieId int64) ([]dto.ReviewResponse, error) {
+
+	reviews, err := s.reviewRepo.GetReviewByMovieId(movieId)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return reviews, nil
 }
-func (s *reviewService) GetReviewByUserEmailId(email string) (*domain.Review, error) {
-	return nil, nil
+func (s *reviewService) GetReviewByUserEmailId(emailId string) ([]dto.ReviewResponse, error) {
+	reviews, err := s.reviewRepo.GetReviewByUserEmailId(emailId)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return reviews, nil
 }
-func (s *reviewService) UpdateReview(review *domain.Review) error {
+func (s *reviewService) UpdateReview(review *domain.Review, reviewId int64) error {
+
+	err := s.reviewRepo.Update(review, reviewId)
+
+	if err !=nil{
+		return err
+	}
+
 	return nil
 }
 func (s *reviewService) DeleteReview(reviewId int64) error {
+
+	err := s.reviewRepo.Delete(reviewId)
+
+	if err != nil{
+		return err
+	}
+
 	return nil
 }
